3c2b866392536ed7c1e6619e3e1624fc327747f2
Jon Robson <jdlrobson@gmail.com>
2026-09-11 22:33:34 -0700

    Update build step

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  plugins: [ vue() ]
+  plugins: [ vue() ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        images: fileURLToPath(new URL('./images.html', import.meta.url))
+      }
+    }
+  }
 });
